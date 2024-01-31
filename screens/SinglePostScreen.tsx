ab732ccf6b0dc82f8d8b68d24535c514e531180e
Sambahangphe <mishek.sambahangphe.303@my.csun.edu>
2024-01-31 15:38:26 -0800

    Added comment section

--- screens/SinglePostScreen.tsx
+++ screens/SinglePostScreen.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet, Pressable, TextInput, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Pressable, TextInput, SafeAreaView, FlatList } from "react-native";
 import HomeScreen from "./HomeScreen";
 
 
 function PostScreen(props: any) {
-    const [text, onChangeText] = React.useState('Useless Text');
+    const [text, ChangeText] = React.useState('');
     const [number, onChangeNumber] = React.useState('');
+    const [comments, setComments] = useState<string[]>([]);
+
+    const handlePostComment = () => {
+        if (text.trim() !== '') {
+          setComments(prevComments => [...prevComments, text]);
+          ChangeText(''); // Clear the TextInput
+        }
+      };
+
     return (
         <SafeAreaView>
         <View>
@@ -13,15 +22,28 @@ function PostScreen(props: any) {
                 <Text style={styles.heading} >{props.route.params.title}</Text>
             </View> 
             <Text style={styles.excerpt}>{props.route.params.excerpt}</Text>
+            <View style={styles.comments}>
+          <Text style={styles.comments}>Comments:</Text>
+          <FlatList
+            data={comments}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.commentItem}>
+                <Text>{item}</Text>
+              </View>
+            )}
+          />
+        </View>
             <TextInput
-            onChangeText={onChangeText}
+            onChangeText={ChangeText}
             value={text}
             style={styles.input}
             />
             <Pressable 
                 style={styles.button}
-                accessibilityLabel="Comment" > 
-                <Text style={styles.buttonText}>Comment</Text>
+                onPress={handlePostComment}
+                accessibilityLabel="Post Comment" > 
+                <Text style={styles.buttonText}>Post Comment</Text>
             </Pressable>   
         </View>
         </SafeAreaView>
@@ -36,6 +58,11 @@ const styles = StyleSheet.create({
         borderBottomWidth: 5,
         borderColor: 'red'
     },
+    comments: {
+        fontSize: 15,
+        borderBottomWidth: 3,
+        borderColor: 'black'
+    },
     input: {
         height: 40,
         margin: 12,
@@ -48,6 +75,12 @@ const styles = StyleSheet.create({
     buttonText: {
         color: 'black'
     },
+    commentItem: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+        marginBottom: 5,
+      },
     button: {
         borderWidth: 2,
         padding: 5
