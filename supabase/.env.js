bd3921119408c39d0c7a07ce34e027e77f8eb08d
Nver Khachoyan <khachoyannver@gmail.com>
2024-01-31 20:47:52 -0800

    Delete supabase/.env.js
    Deleted private keys

--- supabase/.env.js
+++
@@ -1,5 +0,0 @@
-const supabaseUrl = 'https://htcsuyydbpcagzzaaegn.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0Y3N1eXlkYnBjYWd6emFhZWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3NTE2MjAsImV4cCI6MjAyMjMyNzYyMH0.lf76CPuQWlbfM8fznbK_6q8mhQY2yZrhZg7exbH-uZY';
-
-export { supabaseUrl, supabaseAnonKey };
